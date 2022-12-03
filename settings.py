14d8c3eef741178402f6bc8e46e2c7447da2e217
polo <polo>
2022-12-03 17:43:17 +0300

    Minor changes to text strings

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ LOCAL_A = 1040 # chr(1040) = А
 LOCAL_Z = 1071 # chr(1071) = Я
 START_GAME_MSG = 'Игра не начата. Начните ее командой /start'
 GAME_STARTED_MSG = '"Города России"\n Начата новая игра. Напишите город или завершите игру командой /stop'
-HELP_MSG = "Don't worry!"
+HELP_MSG = "Don't worry! :-)"
 GAME_STOPPED_MSG = "Игра завершена. Спасибо!"
 MISSPELLED_OR_REPEAT_MSG = 'Такого города нет, ошибка в написании (Ё пишите через Е) или его уже называли'
